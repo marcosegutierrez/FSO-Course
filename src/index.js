d166cbcdf879b05cd46be6d1e98b1ca43a10ebab
Marcos Gutierrez <marcose.gutierrez@hotmail.com>
2022-02-18 23:27:19 -0300

    Funcion que devuelve una funcion

--- src/index.js
+++ src/index.js
@@ -1,50 +1,25 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      Button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ onClick, text}) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
-
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const setToValue = (newValue) => {
+    setValue(newValue)
   }
 
   return (
     <div>
-      {left}
-      <Button onClick={handleLeftClick} text='left' />
-      <Button onClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={() => setToValue(1000)}>
+        thousand
+      </button>
+      <button onClick={() => setToValue(0)}>
+        reset
+      </button>
+      <button onClick={() => setToValue(value + 1)}>
+        increment
+      </button>
     </div>
   )
 }
